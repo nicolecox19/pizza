b11edd09c6ed1da1f17947b47aa3612fde37f59b
Nicole Cox <nicolecox@Nicoles-MacBook-Pro.local>
2018-10-28 11:53:47 -0700

    Working on the README

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//buisness logic
 function Pizza(quantity, pizzaSize) {
   this.quantity = quantity;
   this.pizzaSize = pizzaSize;
@@ -5,50 +6,49 @@ function Pizza(quantity, pizzaSize) {
   this.meatTopping = [];
 };
 
-Pizza.prototype.calculatePrice = function () {
+Pizza.prototype.calculatePrice = function() {
   var totalPrice = 2 + (this.veggieTopping.length * 1)
 
   if (this.pizzaSize === "small") {
     return totalPrice * this.quantity;
   } else if (this.pizzaSize == "medium") {
-      return (this.pizzaSize + 2) * this.quantity;
-    } else if (this.pizzaSize === "large") {
-      return (totalPrice + 4) * this.quantity;
-    } else if (this.pizzaSize == "x-large") {
-      return (totalPrice + 7) * this.quantity;
-   }
- };
+    return (this.pizzaSize + 2) * this.quantity;
+  } else if (this.pizzaSize === "large") {
+    return (totalPrice + 4) * this.quantity;
+  } else if (this.pizzaSize == "x-large") {
+    return (totalPrice + 8) * this.quantity;
+  }
+};
 
 
 
+//user interface
+$(document).ready(function() {
+  $("form#order-form").submit(function(event) {
+    var quantity = $("input#quantity").val();
+    var pizzaSize = $("select#size").val();
 
- $(document).ready(function() {
-   $("form#order-form").submit(function(event) {
-     var quantity = $("input#quantity").val();
-     var pizzaSize = $("select#size").val();
+    var newPizza = new Pizza(quantity, pizzaSize);
 
-     var newPizza = new Pizza(quantity, pizzaSize);
+    // debugger;
 
- debugger;
-
-     var veggieTopping = $("input.veg:checkbox:checked").each(function() {
-       newPizza.veggieTopping.push(veggieTopping);
-     });
-
-
-     var meatTopping = $("input.meat:checkbox:checked").each(function() {
-       newPizza.meatTopping.push(meatTopping);
-     });
+    var veggieTopping = $("input.veg:checkbox:checked").each(function() {
+      newPizza.veggieTopping.push(veggieTopping);
+    });
 
 
-     var pizzaPrice = newPizza.calculatePrice();
+    var meatTopping = $("input.meat:checkbox:checked").each(function() {
+      newPizza.meatTopping.push(meatTopping);
+    });
+
+
+    var pizzaPrice = newPizza.calculatePrice();
 
 
 
-     $("#price").text(pizzaPrice);
+    $("#price").text(pizzaPrice);
 
-
-     $("#result").show();
-     event.preventDefault();
-   });
- });
+    $("#result").show();
+    event.preventDefault();
+  });
+});
